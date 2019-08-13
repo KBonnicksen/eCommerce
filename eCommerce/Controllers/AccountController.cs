6c417db8073c2cc3ff3cd39ed7b57b201a45f809
Kelsey Bonnicksen <kelseybon@gmail.com>
2019-08-13 09:28:38 -0700

    Created functional registration

---
+++ eCommerce/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerce.Data;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly GameContext _context;
+        //Never create a new context. 
+        //Make it once and then pass it around
+        public AccountController(GameContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(Member m)
+        {
+            await MemberDB.Add(_context, m);
+
+            TempData["Message"] = "You registered successfully";
+
+            //How you redirect to another page!!
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ eCommerce/Data/MemberDB.cs
@@ -0,0 +1,26 @@
+﻿using eCommerce.Data;
+using eCommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce
+{
+
+    public static class MemberDB
+    {
+        /// <summary>
+        /// Adds a member to the database. Returns the member with their memberID
+        /// </summary>
+        /// <param name="context">The database context used</param>
+        /// <param name="m">The member being added to the databaase</param>
+        /// <returns></returns>
+        public async static Task<Member> Add(GameContext context, Member m)
+        {
+            context.Members.Add(m);
+            await context.SaveChangesAsync();
+            return m;
+        }
+    }
+}
